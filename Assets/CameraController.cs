94a99e8a1606b70b005e09875a66a2572840395a
後藤啓文 <info@3uindustry.co.jp>
2019-04-18 23:22:31 +0900

    20190418
    きちんと色をサイズ５の箱には付けられた
    robo kyle にはできない。サイズ１の箱もだめ。
    カメラの高さが上下するのを直したいが、まだ。
    カメラが箱をすり抜けてしまう。

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,28 +4,14 @@ using UnityEngine;
 
 namespace FPS
 {
-
     public class CameraController : MonoBehaviour
     {
+        
+        private void Start()
+        {
 
+        }
 
-        [Range(0.1f, 10f)]
-        //カメラ感度、数値が大きいほどより直感的な操作が可能.
-        public float lookSensitivity = 10f;
-        [Range(0.1f, 1f)]
-        //数値が大きいほどカメラが向きたい方向に向くまでの時間が長くなる.
-        public float lookSmooth = 0.1f;
-
-        public Vector2 MinMaxAngle = new Vector2(-65, 65);
-
-        //private float yRot;
-        private float xRot;
-
-        private float currentYRot;
-        //private float currentXRot;
-
-        private float yRotVelocity;
-        private float xRotVelocity;
 
         void Update()
         {
@@ -33,28 +19,32 @@ namespace FPS
             Vector3 localAngle = mytransform.localEulerAngles;
 
 
-           // yRot += Input.GetAxis("Mouse X") * lookSensitivity; //マウスの移動.
-           xRot -= Input.GetAxis("Mouse Y") * lookSensitivity; //マウスの移動.
-
+           
             if (Input.GetKey("right"))
             {
-                transform.Rotate(new Vector3(0, 1, 0));
-                  
+              transform.Rotate(new Vector3(0, 1, 0));
+                
+
+
             }
             if (Input.GetKey("left"))
             {
-                transform.Rotate(new Vector3(0, -1, 0));
+
+               transform.Rotate(new Vector3(0, -1, 0));
+
+            }
+            if (Input.GetKey("up"))
+            {//離したら水平にもどした方が良いかな
+                transform.Rotate(new Vector3(-1, 0, 0));
+
+            }
+            if (Input.GetKey("down"))
+            {
+                transform.Rotate(new Vector3(1, 0, 0));
 
             }
 
-            xRot = Mathf.Clamp(xRot, MinMaxAngle.x, MinMaxAngle.y);//上下の角度移動の最大、最小.
-
-
-            // currentXRot = Mathf.SmoothDamp(currentXRot, xRot, ref xRotVelocity, lookSmooth);
-            //currentYRot = Mathf.SmoothDamp(currentYRot, yRot, ref yRotVelocity, lookSmooth);
-
-             //transform.rotation = Quaternion.Euler(currentXRot, currentYRot, 0);
-
+           
 
         }
     }
--- Assets/moveController.cs
+++ Assets/moveController.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class moveController : MonoBehaviour
 {
 
-    public float speed = 5.0f;
+    public float speed = 8.0f;
     private GameObject gun;
     void Update()
     {
 
         gun = GameObject.Find("MachineGun_01");
-
+        float basho = gun.transform.position.y;
 
         if (gun.transform.position.y > 1 && gun.transform.position.y < 3)
         {
+            //範囲を超えたら戻す必要あり
+
 
             if (Input.GetKey("w"))
             {
+
                 transform.position += transform.forward * speed * Time.deltaTime;
+                //yだけ変えたくない
             }
             if (Input.GetKey("s"))
             {
