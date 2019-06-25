eda4b6da9e84b53741ce3a5b3fa55b489e2eed77
後藤啓文 <info@3uindustry.co.jp>
2019-06-25 19:48:38 +0900

    20109625
    スタート画面を作ってみた

---
+++ Assets/GameChangeStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameChangeStart : MonoBehaviour
+{
+    private bool GameStart = false;
+    public void GameStartButtonDown()
+    {
+        this.GameStart = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((GameStart))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
